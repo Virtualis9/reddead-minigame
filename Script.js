dc45b22dbe691cb1e579a3153ccf815a704549fc
Christopher Peacock <122057441+Virtualis9@users.noreply.github.com>
2024-05-05 22:23:55 +0100

    target moves when event of 'E' is pressed

--- Script.js
+++ Script.js
@@ -1,9 +1,45 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const container = document.querySelector(".container");
+    console.log(container);
 
+    const wheel = document.querySelector(".wheel");
+    console.log(wheel);
 
-const circle = document.getElementById("circle")
-const line = document.getElementById("line")
+    const target = document.getElementById("target");
+    console.log(target);
 
-function test(e){
+    const line = document.getElementById("line");
+    console.log(line);
 
-    console.log()
-}
+    
+
+    document.addEventListener('keydown', function(event) {
+        if (event.key === 'e') {
+            console.log('E clicked');
+            moveElements();
+            difficulty()
+            
+        }
+
+        function moveElements(){
+            let random = Math.floor(Math.random() * 360) ;
+            wheel.style.transform = "rotate(" + random + "deg)"; 
+            console.log(wheel.style.transform)
+
+        }
+
+        function difficulty(){
+            let speed = Math.floor(Math.random() * 3 ) + 0.5;
+            line.style.transform = "spin(" + speed +"s infinite linear"
+            console.log(line.style.animation)
+            console.log(speed)
+
+        }
+
+        function nextLevel(){
+            
+        }
+
+    });
+
+});
